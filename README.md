2c5e509b8bd2606705a5a627f1b43100edf49902
Stephon Smith <stephonsmith@Stephons-Mac-mini.local>
2022-08-03 15:25:13 -0400

    Updated Project Files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PixelArt_Project_Repo
+
+# This Project Goes Over Writing The Code For The Pixel Art Maker Project. 
+# There is also going to be a link for the recording of the Project along with going over Github
--- project-pixel-art-maker-starter-master 2/designs.js
+++ project-pixel-art-maker-starter-master 2/designs.js
@@ -1,10 +1,58 @@
-// Select color input
-// Select size input
+// Variable To Choose The Size Of The Grid: 
+const sizePicker = document.querySelector('#sizePicker');
 
-// When size is submitted by the user, call makeGrid()
+// Variable Of the Design Canvas: 
+const table = document.querySelector('#pixelCanvas');
 
-function makeGrid() {
+//Default Size: 
+makeGrid(10, 10);
 
-// Your code goes here!
+
+// #1. Submit The Size Of The Grid: 
+
+sizePicker.addEventListener('submit', function (event) {
+
+    // #1. Clear Table:
+       event.preventDefault();
+       
+       while(table.hasChildNodes()){
+            table.removeChild(table.firstChild)
+       }
+
+    // #2. Apply The User's Size:
+    const height = document.querySelector('#inputHeight');
+    const width = document.querySelector('#inputWidth');
+    makeGrid(height.value, width.value);
+
+});
+
+
+// #2. When Grid Size is submitted by the user, call makeGrid():
+function makeGrid(height, width) {
+
+        for(let y = 0; y < height; y++){
+
+              let row = table.insertRow(y);
+
+              for (let x = 0; x < width; x++){
+
+                let cell = row.insertCell(x);
+
+                // Set Colors To Cell: 
+
+                cell.style.backgroundColor = "white";
+                cell.addEventListener('click', function (event) {
+
+                    event.preventDefault();
+                    const color = document.querySelector('#colorPicker');
+                    cell.style.backgroundColor = color.value;
+
+                });
+
+              }
+
+        }
+
+
 
 }
